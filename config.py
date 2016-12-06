aef1858fdd90a83c151dd156ff43829c723567af
steveh110 <steveh110@gmail.com>
2016-12-06 09:24:14 +0100

    Update config.py

--- config.py
+++ config.py
@@ -9,5 +9,5 @@ Voice = "julie"
 
 #Traduction
 # https://datamarket.azure.com/account
-Azure_client_id = "048ad8bd-a985-4a98-a514-e3a2d18cd7d2"
-Azure_client_secret = "XIOpUP9qFG0qyVXqJzLZ4Ec4Do0fc6XO0bDG26c1uuw"
+Azure_client_id = "????????"
+Azure_client_secret = "?????????????????"
